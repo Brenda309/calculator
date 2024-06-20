43f4496407cf5fe758d1099526f16af11d91e8f7
Brenda <brewihogora23@gmail.com>
2024-06-20 13:21:18 +0200

    Add button functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -32,26 +32,82 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  buttonPressed(){
+  String output = "0";
+String _output = "0";
+double num1 = 0;
+double  num2 = 0;
+String operand = "";
 
+  buttonPressed(String buttonText){
+if(buttonText == "CLEAR"){
+  _output = "0";
+  num1 = 0;
+  num2 = 0;
+  String operand= "";
+}else if (buttonText == "+" ||
+buttonText == "-" ||
+buttonText == "/" ||
+buttonText == "*"){
+  num1 = double.parse(output);
+  operand = buttonText;
+  _output= "0";
+}else if ( buttonText == "."){
+  if(_output.contains(".")){
+    print ('already contains a decimal');
+  }else{
+    _output = _output + buttonText;
   }
-  Widget buildingButton(String buttonText){
+}else if (buttonText == "="){
+  num2 = double.parse(output);
+  if(operand == "+"){
+    _output = (num1 + num2).toString();
+  }
+  if(operand == "-"){
+    _output = (num1 - num2).toString();
+  }
+
+  if(operand == "*"){
+    _output = (num1 * num2).toString();
+  }
+
+  if(operand == "/"){
+    _output = (num1 / num2).toString();
+  }
+num1 = 0;
+  num2 = 0;
+  operand = "";
+}else{
+  _output = _output + buttonText;
+}
+print(_output);
+setState(() {
+  output = double.parse(_output).toStringAsFixed(1);
+});
+  }
+  Widget buildingButton(String buttonText) {
     return Expanded(
-        child: SizedBox(
-          height: 70.0,
-          child: Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: OutlinedButton(
-              child: Text(
-                buttonText,
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+      child: SizedBox(
+        height: 120.0,
+        child: Padding(
+          padding: const EdgeInsets.all(13.0),
+          child: OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20), // Rectangle shape with no rounded corners
               ),
-              onPressed: () => buttonPressed(),
+              side: BorderSide(width: 3.0, color: Colors.brown), // Customize border if needed
+            ),
+            onPressed: () => buttonPressed(buttonText),
+            child: Text(
+              buttonText,
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
             ),
           ),
-        )
+        ),
+      ),
     );
   }
+
   @override
 Widget build (BuildContext context){
   return Scaffold(
@@ -64,9 +120,9 @@ Widget build (BuildContext context){
         children: <Widget>[
           Container(
             alignment: Alignment.centerRight,
-            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal:12.0),
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal:12.0),
             child: Text(
-                '0',
+                output,
                 style:TextStyle(
                   fontSize: 48.0,
                   fontWeight: FontWeight.bold,
